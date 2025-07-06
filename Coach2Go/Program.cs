8269fd7cf94d7cb69387c28876286ccd7d46e9db
star <A.PETRASHKO-2022@hull.ac.uk>
2025-07-06 22:16:57 +0100

    Resolved merge conflict in Program.cs

--- Coach2Go/Program.cs
+++ Coach2Go/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Coach2Go;
 using MudBlazor.Services;
-using Blazored.LocalStorage; 
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,21 +10,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-<<<<<<< HEAD
     BaseAddress = new Uri("http://localhost:5136")
-=======
-    BaseAddress = new Uri("http://localhost:5136") 
->>>>>>> e02b59bb5928a45dfbb571013add94516414e195
 });
 
 builder.Services.AddMudServices();
-
-<<<<<<< HEAD
 builder.Services.AddScoped<AuthService>();
-
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
-=======
-await builder.Build().RunAsync();
->>>>>>> e02b59bb5928a45dfbb571013add94516414e195
