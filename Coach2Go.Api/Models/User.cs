ae1bbb63663b43443a743c36eebafbd4eb0343a1
star <A.PETRASHKO-2022@hull.ac.uk>
2025-07-06 22:22:31 +0100

    Resolved merge conflict in User.cs

--- Coach2Go.Api/Models/User.cs
+++ Coach2Go.Api/Models/User.cs
@@ -8,9 +8,7 @@ namespace Coach2Go.Api.Models
 
         public string Username { get; set; } = default!;
         public string Email { get; set; } = default!;
-
-<<<<<<< HEAD
-        public string? GoogleId { get; set; } 
+        public string? GoogleId { get; set; }
 
         public string Goal { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
@@ -20,8 +18,7 @@ namespace Coach2Go.Api.Models
 
         [ForeignKey("WorkoutPlanId")]
         public WorkoutPlan? WorkoutPlan { get; set; }
-=======
-        public List<WorkoutPlan> Plans { get; set; } = new(); 
->>>>>>> e02b59bb5928a45dfbb571013add94516414e195
+
+        public List<WorkoutPlan> Plans { get; set; } = new();
     }
 }
